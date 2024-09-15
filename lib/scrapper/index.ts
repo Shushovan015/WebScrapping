0c8ddd0dfaf4137783bb881337cbdbcd3fbd0335
Shushovan shakya <shushovan015@gmail.com>
2024-09-15 14:08:26 +0200

    extact function for the rpoducts

--- lib/scrapper/index.ts
+++ lib/scrapper/index.ts
@@ -1,5 +1,6 @@
 import axios from "axios";
 import * as cheerio from "cheerio";
+import { extractCurrency, extractPrice } from "../utils";
 export async function scrapeAmazonProduct(url: string) {
   if (!url) return;
   //BrightData proxy configuration
@@ -19,11 +20,51 @@ export async function scrapeAmazonProduct(url: string) {
   };
 
   try {
-    console.log("here i am");
     //Fetch the Product page
     const response = await axios.get(url, options);
     const $ = cheerio.load(response.data);
     const title = $("#productTitle").text().trim();
+    const currentPrice = extractPrice(
+      $(".priceToPay span.a-price-whole"),
+      $("a.size.base.a-color-price"),
+      $(".a-button-selected .a-color-base"),
+      $(".a-price.a-text-price")
+    );
+    const originalPrice = extractPrice(
+      $("#priceblock_ourprice"),
+      $(".a-price.a-text-price span.a-offscreen"),
+      $("#listPrice"),
+      $("#priceblock_dealprice"),
+      $(".a-size-base.a-color-price")
+    );
+
+    const outOfStock =
+      $("#availability span").text().trim().toLowerCase() ===
+      "currently unavailable";
+
+    const images =
+      $("#imgBlkFront").attr("data-a-dynamic-image") ||
+      $("#landingImage").attr("data-a-dynamic-image") ||
+      "{}";
+    const imageUrls = Object.keys(JSON.parse(images));
+    const currency = extractCurrency($(".a-price-symbol"));
+    const discountRate = $(".savingsPercentage").text().replace(/[-%]/g, "");
+    // construct data object with scraped info
+    const data = {
+      url,
+      currency: currency || "$",
+      image: imageUrls[0],
+      title,
+      currentPrice: Number(currentPrice),
+      originalPrice: Number(originalPrice),
+      priceHistory: [],
+      discountRate: Number(discountRate),
+      category: "category",
+      reviewsCount: 0,
+      stars: 0,
+      isOutOfStock: outOfStock,
+    };
+    console.log(data, "data");
   } catch (error: any) {
     throw new Error(`Failed to scrap product: ${error.message}`);
   }
